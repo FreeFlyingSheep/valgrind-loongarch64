7fb85fcd7800dee5092dc854e18082caa35ad00c
Feiyang Chen <chenfeiyang@loongson.cn>
2022-03-23 16:28:54 +0800

    loongarch64: Add support for cachegrind

--- cachegrind/cg_arch.c
+++ cachegrind/cg_arch.c
@@ -477,7 +477,10 @@ configure_caches(cache_t *I1c, cache_t *D1c, cache_t *LLc,
 
 #elif defined(VGA_loongarch64)
 
-   /* TODO */
+   // Set caches to default (for LOONGARCH64 - 3A5000)
+   *I1c = (cache_t) {    65536,  4, 64 };
+   *D1c = (cache_t) {    65536,  4, 64 };
+   *LLc = (cache_t) {   262144, 16, 64 };
 
 #else
 
